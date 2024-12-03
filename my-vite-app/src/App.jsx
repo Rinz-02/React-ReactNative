e3fb6f514654e03dc72204c7673d7e06c0d6de5d
Rinz-02 <willhan11122@gmail.com>
2024-12-03 14:52:29 +0630

    My first day working on React

---
+++ my-vite-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './Welcome'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <WelcomeMessage/>
+  )
+}
+
+export default App
---
+++ my-vite-app/src/Welcome.jsx
@@ -0,0 +1,17 @@
+
+function WelcomeMessage() {
+    return(
+        <div>
+            <section>
+                <h1> This is Introduction</h1>
+
+                <p>
+                    This is React Introduction, Today we'll learn about component!
+
+                </p>
+            </section>
+        </div>
+    )
+}
+
+export default WelcomeMessage;
